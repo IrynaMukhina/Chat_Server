45a18c40886baa657a22c8fe950c7dd30905624a
IrynaMukhina <imukhina@griddynamics.com>
2019-12-13 13:22:26 +0200

    emit chat to all users

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const { NODE_ENV, MONGODB_LOCAL, MONGODB_REMOTE } = process.env;
 const db_uri = NODE_ENV === 'production' ? MONGODB_REMOTE : MONGODB_LOCAL;
 const db = mongoose.connection;
 
+mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.connect(db_uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
